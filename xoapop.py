ed11446e89b2781c5a5c84841a1ad153b65c08d1
Xoapop <149486414+Xoapop@users.noreply.github.com>
2023-10-31 08:49:38 -0400

    Create xoapop.py

---
+++ xoapop.py
@@ -0,0 +1,28 @@
+import os, base64, codecs
+
+# repalce these vars with the obf code
+magic = 
+love = 
+god = 
+destiny = 
+joy = 
+trust = 
+
+
+# opens txt decodes it from base64 and writes to the txt
+with open('decoded.txt', 'w') as f:
+        thang = base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'
+        print(thang, file=f)
+
+with open('decoded.txt', 'r') as file:
+    data = file.read()
+
+os.remove("decoded.txt")
+
+# removes the b" and reformats the code and writes to txt
+decoded_data = data[2:-1]
+decoded_data = decoded_data.replace('\\\\', '\\').replace('\\t', '\t').replace('\\n', '\n')
+
+with open('formatted.txt', 'w') as file:
+    file.write(decoded_data)
+
